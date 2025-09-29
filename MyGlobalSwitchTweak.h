5660f587959315d049cfcdc92dff13cc7cd0d01e
A先生 <142523398+axs66@users.noreply.github.com>
2025-09-29 16:42:41 +0800

    Create MyGlobalSwitchTweak.h

---
+++ MyGlobalSwitchTweak.h
@@ -0,0 +1,8 @@
+#ifndef MY_GLOBAL_SWITCH_TWEAK_H
+#define MY_GLOBAL_SWITCH_TWEAK_H
+
+#define MGSTPrefsDomain "com.example.myglobalswitch"
+#define MGSTPrefsChangedNotification "com.example.myglobalswitch/ReloadPrefs"
+
+#endif /* MY_GLOBAL_SWITCH_TWEAK_H */
+
